51b8ddfc1a40324a60588298aaa5fbd6a6a7da3e
mikal <mikal>
2000-11-23 21:18:57 +0000

    Mikal: Updated the doco files for the release now that I believe that I have g3 and g4 tiff working as expected.

--- testdata/output.c
+++ testdata/output.c
@@ -23,7 +23,444 @@ int main(int argc, char *argv[]){
 
 
 
+"g3-B-lsb2msb-contig-1.tif"
+    );
+
+    imagebox(demo, currPage, 0, 0, currPage->height / 2, currPage->width, 
+      filename, gImageTiff);
+
+    textbox(demo, currPage, 20, 20, 100, 30, filename);
+
+
+    // Create a page
+    currPage = pdfpage(demo, gPageSizeA4);
+
+    // Insert some images
+    sprintf(filename, "%s",
+
+
+
+
+
+"g3-B-lsb2msb-contig-1000.tif"
+    );
+
+    imagebox(demo, currPage, 0, 0, currPage->height / 2, currPage->width, 
+      filename, gImageTiff);
+
+    textbox(demo, currPage, 20, 20, 100, 30, filename);
+
+
+    // Create a page
+    currPage = pdfpage(demo, gPageSizeA4);
+
+    // Insert some images
+    sprintf(filename, "%s",
+
+
+
+
+
+"g3-B-lsb2msb-contig-1000000.tif"
+    );
+
+    imagebox(demo, currPage, 0, 0, currPage->height / 2, currPage->width, 
+      filename, gImageTiff);
+
+    textbox(demo, currPage, 20, 20, 100, 30, filename);
+
+
+    // Create a page
+    currPage = pdfpage(demo, gPageSizeA4);
+
+    // Insert some images
+    sprintf(filename, "%s",
+
+
+
+
+
+"g3-B-msb2lsb-contig-1.tif"
+    );
+
+    imagebox(demo, currPage, 0, 0, currPage->height / 2, currPage->width, 
+      filename, gImageTiff);
+
+    textbox(demo, currPage, 20, 20, 100, 30, filename);
+
+
+    // Create a page
+    currPage = pdfpage(demo, gPageSizeA4);
+
+    // Insert some images
+    sprintf(filename, "%s",
+
+
+
+
+
+"g3-B-msb2lsb-contig-1000.tif"
+    );
+
+    imagebox(demo, currPage, 0, 0, currPage->height / 2, currPage->width, 
+      filename, gImageTiff);
+
+    textbox(demo, currPage, 20, 20, 100, 30, filename);
+
+
+    // Create a page
+    currPage = pdfpage(demo, gPageSizeA4);
+
+    // Insert some images
+    sprintf(filename, "%s",
+
+
+
+
+
+"g3-B-msb2lsb-contig-1000000.tif"
+    );
+
+    imagebox(demo, currPage, 0, 0, currPage->height / 2, currPage->width, 
+      filename, gImageTiff);
+
+    textbox(demo, currPage, 20, 20, 100, 30, filename);
+
+
+    // Create a page
+    currPage = pdfpage(demo, gPageSizeA4);
+
+    // Insert some images
+    sprintf(filename, "%s",
+
+
+
+
+
+"g3-L-lsb2msb-contig-1.tif"
+    );
+
+    imagebox(demo, currPage, 0, 0, currPage->height / 2, currPage->width, 
+      filename, gImageTiff);
+
+    textbox(demo, currPage, 20, 20, 100, 30, filename);
+
+
+    // Create a page
+    currPage = pdfpage(demo, gPageSizeA4);
+
+    // Insert some images
+    sprintf(filename, "%s",
+
+
+
+
+
+"g3-L-lsb2msb-contig-1000.tif"
+    );
+
+    imagebox(demo, currPage, 0, 0, currPage->height / 2, currPage->width, 
+      filename, gImageTiff);
+
+    textbox(demo, currPage, 20, 20, 100, 30, filename);
+
+
+    // Create a page
+    currPage = pdfpage(demo, gPageSizeA4);
+
+    // Insert some images
+    sprintf(filename, "%s",
+
+
+
+
+
+"g3-L-lsb2msb-contig-1000000.tif"
+    );
+
+    imagebox(demo, currPage, 0, 0, currPage->height / 2, currPage->width, 
+      filename, gImageTiff);
+
+    textbox(demo, currPage, 20, 20, 100, 30, filename);
+
+
+    // Create a page
+    currPage = pdfpage(demo, gPageSizeA4);
+
+    // Insert some images
+    sprintf(filename, "%s",
+
+
+
+
+
+"g3-L-msb2lsb-contig-1.tif"
+    );
+
+    imagebox(demo, currPage, 0, 0, currPage->height / 2, currPage->width, 
+      filename, gImageTiff);
+
+    textbox(demo, currPage, 20, 20, 100, 30, filename);
+
+
+    // Create a page
+    currPage = pdfpage(demo, gPageSizeA4);
+
+    // Insert some images
+    sprintf(filename, "%s",
+
+
+
+
+
+"g3-L-msb2lsb-contig-1000.tif"
+    );
+
+    imagebox(demo, currPage, 0, 0, currPage->height / 2, currPage->width, 
+      filename, gImageTiff);
+
+    textbox(demo, currPage, 20, 20, 100, 30, filename);
+
+
+    // Create a page
+    currPage = pdfpage(demo, gPageSizeA4);
+
+    // Insert some images
+    sprintf(filename, "%s",
+
+
+
+
+
+"g3-L-msb2lsb-contig-1000000.tif"
+    );
+
+    imagebox(demo, currPage, 0, 0, currPage->height / 2, currPage->width, 
+      filename, gImageTiff);
+
+    textbox(demo, currPage, 20, 20, 100, 30, filename);
+
+
+    // Create a page
+    currPage = pdfpage(demo, gPageSizeA4);
+
+    // Insert some images
+    sprintf(filename, "%s",
+
+
+
+
+
+"g4-B-lsb2msb-contig-1.tif"
+    );
+
+    imagebox(demo, currPage, 0, 0, currPage->height / 2, currPage->width, 
+      filename, gImageTiff);
+
+    textbox(demo, currPage, 20, 20, 100, 30, filename);
+
+
+    // Create a page
+    currPage = pdfpage(demo, gPageSizeA4);
+
+    // Insert some images
+    sprintf(filename, "%s",
+
+
+
+
+
+"g4-B-lsb2msb-contig-1000.tif"
+    );
+
+    imagebox(demo, currPage, 0, 0, currPage->height / 2, currPage->width, 
+      filename, gImageTiff);
+
+    textbox(demo, currPage, 20, 20, 100, 30, filename);
+
+
+    // Create a page
+    currPage = pdfpage(demo, gPageSizeA4);
+
+    // Insert some images
+    sprintf(filename, "%s",
+
+
+
+
+
+"g4-B-lsb2msb-contig-1000000.tif"
+    );
+
+    imagebox(demo, currPage, 0, 0, currPage->height / 2, currPage->width, 
+      filename, gImageTiff);
+
+    textbox(demo, currPage, 20, 20, 100, 30, filename);
+
+
+    // Create a page
+    currPage = pdfpage(demo, gPageSizeA4);
+
+    // Insert some images
+    sprintf(filename, "%s",
+
+
+
+
+
+"g4-B-msb2lsb-contig-1.tif"
+    );
+
+    imagebox(demo, currPage, 0, 0, currPage->height / 2, currPage->width, 
+      filename, gImageTiff);
+
+    textbox(demo, currPage, 20, 20, 100, 30, filename);
+
+
+    // Create a page
+    currPage = pdfpage(demo, gPageSizeA4);
+
+    // Insert some images
+    sprintf(filename, "%s",
+
+
+
+
+
 "g4-B-msb2lsb-contig-1000.tif"
+    );
+
+    imagebox(demo, currPage, 0, 0, currPage->height / 2, currPage->width, 
+      filename, gImageTiff);
+
+    textbox(demo, currPage, 20, 20, 100, 30, filename);
+
+
+    // Create a page
+    currPage = pdfpage(demo, gPageSizeA4);
+
+    // Insert some images
+    sprintf(filename, "%s",
+
+
+
+
+
+"g4-B-msb2lsb-contig-1000000.tif"
+    );
+
+    imagebox(demo, currPage, 0, 0, currPage->height / 2, currPage->width, 
+      filename, gImageTiff);
+
+    textbox(demo, currPage, 20, 20, 100, 30, filename);
+
+
+    // Create a page
+    currPage = pdfpage(demo, gPageSizeA4);
+
+    // Insert some images
+    sprintf(filename, "%s",
+
+
+
+
+
+"g4-L-lsb2msb-contig-1.tif"
+    );
+
+    imagebox(demo, currPage, 0, 0, currPage->height / 2, currPage->width, 
+      filename, gImageTiff);
+
+    textbox(demo, currPage, 20, 20, 100, 30, filename);
+
+
+    // Create a page
+    currPage = pdfpage(demo, gPageSizeA4);
+
+    // Insert some images
+    sprintf(filename, "%s",
+
+
+
+
+
+"g4-L-lsb2msb-contig-1000.tif"
+    );
+
+    imagebox(demo, currPage, 0, 0, currPage->height / 2, currPage->width, 
+      filename, gImageTiff);
+
+    textbox(demo, currPage, 20, 20, 100, 30, filename);
+
+
+    // Create a page
+    currPage = pdfpage(demo, gPageSizeA4);
+
+    // Insert some images
+    sprintf(filename, "%s",
+
+
+
+
+
+"g4-L-lsb2msb-contig-1000000.tif"
+    );
+
+    imagebox(demo, currPage, 0, 0, currPage->height / 2, currPage->width, 
+      filename, gImageTiff);
+
+    textbox(demo, currPage, 20, 20, 100, 30, filename);
+
+
+    // Create a page
+    currPage = pdfpage(demo, gPageSizeA4);
+
+    // Insert some images
+    sprintf(filename, "%s",
+
+
+
+
+
+"g4-L-msb2lsb-contig-1.tif"
+    );
+
+    imagebox(demo, currPage, 0, 0, currPage->height / 2, currPage->width, 
+      filename, gImageTiff);
+
+    textbox(demo, currPage, 20, 20, 100, 30, filename);
+
+
+    // Create a page
+    currPage = pdfpage(demo, gPageSizeA4);
+
+    // Insert some images
+    sprintf(filename, "%s",
+
+
+
+
+
+"g4-L-msb2lsb-contig-1000.tif"
+    );
+
+    imagebox(demo, currPage, 0, 0, currPage->height / 2, currPage->width, 
+      filename, gImageTiff);
+
+    textbox(demo, currPage, 20, 20, 100, 30, filename);
+
+
+    // Create a page
+    currPage = pdfpage(demo, gPageSizeA4);
+
+    // Insert some images
+    sprintf(filename, "%s",
+
+
+
+
+
+"g4-L-msb2lsb-contig-1000000.tif"
     );
 
     imagebox(demo, currPage, 0, 0, currPage->height / 2, currPage->width, 
